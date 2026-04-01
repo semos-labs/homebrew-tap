eb84a04f50f8d5ce13f9361c98512c05c976cd05
Nick Skriabin <767890+nick-skriabin@users.noreply.github.com>
2026-04-01 08:20:12 +0100

    Update SHA256 checksum for arm version

--- Casks/attyx.rb
+++ Casks/attyx.rb
@@ -3,7 +3,7 @@ cask "attyx" do
 
   on_arm do
     url "https://github.com/semos-labs/attyx/releases/download/v#{version}/attyx-darwin-arm64.zip"
-    sha256 "20e5eb1b1d06cee9d976a40d319c123d905157fada42780874e7827bd487486f"
+    sha256 "144fa16c6c238635c8795193daf72b2b183bb7767c662813c5f4a900d7c62ebd"
   end
   on_intel do
     url "https://github.com/semos-labs/attyx/releases/download/v#{version}/attyx-darwin-x64.zip"
